6bcf075693fc258411f4b71124467665199e5e7d
dennissammy77 <dennissammy77@gmail.com>
2025-06-24 13:38:58 +0300

    added metadata

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from datetime import datetime
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 
 class User(db.Model):
     __tablename__ = 'users'
