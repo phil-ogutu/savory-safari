6f48ac628b9705d4c05f4316259ab9ad4bad9026
dennissammy77 <dennissammy77@gmail.com>
2025-06-27 00:04:07 +0300

    refactor: ui component (button)

--- frontend/src/components/Post/CommentButton.jsx
+++ frontend/src/components/Post/CommentButton.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment } from "@fortawesome/free-regular-svg-icons"; // <-- Add this line
 
-const LikeButton = ({ postId }) => {
+const CommentButton = ({ postId }) => {
   return <button className="text-gray-600 hover:text-gray-700 text-2xl"><FontAwesomeIcon icon ={faComment}/></button>;
 };
 
-export default LikeButton;
+export default CommentButton;
