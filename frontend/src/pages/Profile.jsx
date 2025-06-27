d8bca9663ab308eb99a690c6c02be8877d11195a
Winnie Siriba <winniemoraa234@gmail.com>
2025-06-27 10:17:48 +0300

    removes message button

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -236,11 +236,7 @@ export default function ProfilePage() {
         </p>
       </div>
 
-      {/* Messages Button */}
-      <button className="fixed bottom-8 right-8 bg-blue-600 text-white p-3 rounded-full shadow-lg hover:bg-blue-700">
-        <MessageSquare size={20} />
-        <span className="ml-2 text-sm">Messages</span>
-      </button>
+      
     </div>
-  );
+
 }
